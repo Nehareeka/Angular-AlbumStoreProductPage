009bd1fa1dca6f6e525b60d15e35e5136fb97aa2
ICEPOR\nrayat <nrayat@ivycomptech.com>
2018-05-31 19:04:49 +0530

    Using Interfaces to Describe Data
    Signed-off-by: ICEPOR\nrayat <nrayat@ivycomptech.com>

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Response } from '@angular/http';
 import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
-  constructor(private _http: Http) { }
+  constructor(private _http: HttpClient) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl)
-    .pipe(map(response => response.json()));
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get<Album>(this._albumUrl)
+    .pipe(map(response => <Album>response));
   }
 }
