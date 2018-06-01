907dedc025a658738521452fa3d7e7f17ca1966a
ICEPOR\nrayat <nrayat@ivycomptech.com>
2018-06-01 11:48:48 +0530

    Adding the Tracklisting Component Part 1

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Track } from '../track';
+import { Album } from '../album';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+trackingList: any;
+albumInfo: Album;
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+    this.trackingList = this._productService.getAlbum(1)
+    .subscribe(response =>  this.albumInfo = response);
+  }
+
+}
